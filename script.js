0415253ad192a28d683d8396bcd97dde622701ad
Vladimir Bernatsky <vvbernacky@gmail.com>
2020-03-19 14:59:04 +0300

    feat: add form action

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let selectCategory = function () {
         let listItems = Array.from(document.querySelectorAll('.portfolio__list__item'));
         let currentCategory = this.dataset.category;
         let list = [];
-        let arr = [];
         for (let a = 0; a <= listItems.length - 1; a++) {
             if (listItems[a].dataset.category === currentCategory.replace('cat-', '')) {
                 list.unshift(listItems[a]);
@@ -28,3 +27,55 @@ let selectCategory = function () {
 filterButtons.forEach(el => {
     el.addEventListener('click', selectCategory, false)
 })
+
+//form
+let form = document.querySelector('#quoteForm');
+let formControls = document.querySelectorAll('.form__controller');
+let generateMessage = (data) => {
+    let messageWrapper = document.createElement("div");
+    messageWrapper.className = "message__wrapper";
+    let message = document.createElement("div");
+    message.className = "message";
+    data.forEach(item => {
+        if (item.attr === 'subject') {
+            if (item.value !== '') {
+                message.insertAdjacentHTML('beforeend', '<div class="message__row"><div class="label">' + item.attr + ':</div><div class="value">' + item.value + '</div></div>')
+            } else {
+                message.insertAdjacentHTML('beforeend', '<div class="message__row"><div class="value">Без темы</div></div>')
+            }
+        }
+        if (item.attr === 'description') {
+            if (item.value !== '') {
+                message.insertAdjacentHTML('beforeend', '<div class="message__row"><div class="label">' + item.attr + ':</div><div class="value">' + item.value + '</div></div>')
+            } else {
+                message.insertAdjacentHTML('beforeend', '<div class="message__row"><div class="value">Без описания</div></div>')
+            }
+        }
+    })
+    message.insertAdjacentHTML('afterbegin', '<div class="message__row">The letter was sent</div>');
+    message.insertAdjacentHTML('beforeend', '<div class="message__row"><button class="message__action">OK</button></div>');
+    messageWrapper.appendChild(message);
+    return messageWrapper;
+}
+let closeModal = (e) => {
+    if (e.target.classList.contains('message__action')) {
+        form.reset();
+        let messageElement = document.querySelector('.message__wrapper');
+        messageElement.classList.remove('visible');
+        setTimeout(() => {
+            messageElement.remove()
+        }, 1000);
+    }
+}
+form.onsubmit = (e) => {
+    let formData = [];
+    e.preventDefault();
+    Array.from(formControls).forEach(item => {
+        formData.push({attr: item.getAttribute('name'), value: item.value})
+    });
+    document.querySelector('.contacts').insertBefore(generateMessage(formData), document.querySelector('.contacts__wrapper'));
+    setTimeout(() => {
+        document.querySelector('.message__wrapper').classList.add('visible');
+    }, 100)
+}
+document.querySelector('.contacts').addEventListener('click', closeModal);
